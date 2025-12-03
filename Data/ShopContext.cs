c5d7d0fbd1804071fb356b19d08495ade368baea
DESKTOP-B62L5TF\jimcr <jim.craig43@hotmail.com>
2025-12-03 20:43:14 -0300

    Chapter 6: Setting Up a Product Database with SQLite

---
+++ Data/ShopContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using storefront.Models;
+
+namespace storefront.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
