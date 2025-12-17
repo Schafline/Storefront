5984607cdb196e518649528b0899968b79e02675
DESKTOP-B62L5TF\jimcr <jim.craig43@hotmail.com>
2025-12-17 17:49:29 -0300

    Chapter 7: Scaffolding the Admin Pages

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-
-using Storefront.Data;
 using Microsoft.EntityFrameworkCore;
+using Storefront.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -14,7 +14,6 @@ builder.Services.AddDbContext<ShopContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("ShopDbConnection"));
 });
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
