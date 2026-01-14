1eaee1a14622a6d2036edfc058326a7f0945b03c
DESKTOP-B62L5TF\jimcr <jim.craig43@hotmail.com>
2026-01-14 18:39:40 -0300

    Chapter 9: Securing the Admin Pages

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Storefront.Data;
 
@@ -6,7 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration
     .AddUserSecrets<Program>();  // This line loads user secrets
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Admin");
+    options.Conventions.AllowAnonymousToPage("/Admin/Login");
+});
+
 
 var culture = new CultureInfo("en-US");
 CultureInfo.DefaultThreadCurrentCulture = culture;
@@ -16,13 +22,21 @@ builder.Services.AddDbContext<ShopContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("ShopDbConnection"));
 });
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Admin/Login";
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see 
+    // https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -30,9 +44,12 @@ app.UseHttpsRedirection();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapStaticAssets();
+
 app.MapRazorPages()
    .WithStaticAssets();
 
