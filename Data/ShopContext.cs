188001e07e488720ff8b88c943f19794d54e806c
DESKTOP-B62L5TF\jimcr <jim.craig43@hotmail.com>
2026-01-21 15:25:05 -0300

    Chapter 10: Add the Order Confirmation Page

--- Data/ShopContext.cs
+++ Data/ShopContext.cs
@@ -12,5 +12,7 @@ namespace Storefront.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
