395bc17480f1b2a7eac6851d213a958059ecac79
DESKTOP-B62L5TF\jimcr <jim.craig43@hotmail.com>
2026-02-12 10:26:21 -0300

    orders first cut

---
+++ Models/OrderStatus.cs
@@ -0,0 +1,12 @@
+namespace Storefront.Models
+{
+    public enum OrderStatus
+    {
+        Pending,
+        Paid,
+        Processing,
+        Shipped,
+        Completed,
+        Cancelled
+    }
+}
